a5826fc610ad6b8d120b07501bcfe2add09ab63a
Roman <movel22@gmail.com>
2017-07-26 09:27:08 +0000

    Finish project

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 'use strict';
 
   function PressEnter(your_text, your_event) {
-		  if(your_text != "" && your_event.keyCode == 13)
-		   getWiki();
-		}
-
-
+	  if(your_text != "" && your_event.keyCode == 13)
+	   getWiki();
+	}
   
   let searchBtn = document.getElementById("button");
   searchBtn.addEventListener("click", getWiki);
   
   let textSearch = document.getElementById("text");
-  let searchTitle = document.getElementById("title");
-  let container = document.getElementById("container");
-  //let aLinks = document.getElementsByTagName("a");
-  let titleDivs = document.getElementsByClassName("block");
-  let scrollBtn = document.getElementById("scroll");
-  
-  
 
   function getWiki() {
-    
-    // var page = 'https://en.wikipedia.org/?curid=';
     
     event.preventDefault();
     let rdyString = "";
@@ -34,65 +23,27 @@
         rdyString += queryString[i];
       }
     }
-    // textSearchCheck(rdyString, queryString);
-    // styleContainer();
-              
-    let api = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch='
-                + textSearch
-                + '&gsrlimit=10&prop=extracts&exintro&explaintext&exchars=140&exlimit=max&origin=*';
     
-    let xhr = new XMLHttpRequest();
+    let api = 'https://en.wikipedia.org/w/api.php?action=opensearch&search='
+               + rdyString
+               + '&format=json&origin=*';
     
-    // xhr.open('GET', api, true);
-    // xhr.send();
-    // xhr.onreadystatechange = function() {
-    //   if (xhr.readyState === 4 && xhr.status === 200) {
-    //     let data = xhr.responseText;
-    //     let json = JSON.parse(data);
-    //     let keys = Object.keys(json.query.pages);
-    //     for (let i = 0; i < keys.length; i++) {
-    //       let data = xhr.responseText;
-    //       let json = JSON.parse(data);
-    //       let keys = Object.keys(json.query.pages);
-    //       for (let i = 0; i < keys.length; i++) {
-    //         // titleDivs[i].childNodes[1].innerHTML = (json.query.pages[keys[i]].title.search("(disambiguation)") ? json.query.pages[keys[i]].title.replace("(disambiguation)", ""):json.query.pages[keys[i]].title) + "<br>" + json.query.pages[keys[i]].extract;
-    //         // titleDivs[i].childNodes[1].href = "https://en.wikipedia.org/wiki/" + json.query.pages[keys[i]].title;
-    //         // titleDivs[i].childNodes[1].setAttribute("class", "links");
-    //         // checkBlankDescrip(i, json.query.pages[keys[i]].extract, json.query.pages[keys[i]].title);
-            
-    //         // let div = document.createElement('div');
-    //         // div.className = "wiki_extract";
-    //         // div.innerHTML = (json.query.pages[keys[i]].title.search("(disambiguation)") ? json.query.pages[keys[i]].title.replace("(disambiguation)", ""):json.query.pages[keys[i]].title) + "<br>" + json.query.pages[keys[i]].extract;
-    //         // div.href = "https://en.wikipedia.org/wiki/" + json.query.pages[keys[i]].title;
-    //         // div.setAttribute("class", "links");
-    //         console.log(json());
-    //       }
-    //     }
-    //   }
-    // };
+    let myList = document.querySelector('ul');
+    while (myList.firstChild) {
+        myList.removeChild(myList.firstChild);
+    }
     
-    
-    
-    
+    /* global fetch */
+    fetch(api)
+    .then(function(response) { return response.json(); })
+    .then(function(data) {
+      for (let i = 0; i < 10; i++) {
+        let listItem = document.createElement('li');
+        listItem.innerHTML = "<blockquote class='bg-info'><a href="
+                  + data[3][i] + "><p>" 
+                  + data[1][i] + "</p><footer>" 
+                  + data[2][i] + "</footer></a></blockquote>";
+        myList.appendChild(listItem);
+      }
+    });
   }
-  
-function checkBlankDescrip(i, extract, title) {
-  if (extract.length < 4) {
-    return titleDivs[i].childNodes[1].innerHTML = title + "<br>" + "Click here for more information on " + title + ".";
-  } 
-}  
-  
-function textSearchCheck(rdyString, queryString) {
-  textSearch.value = "";
-  queryString === "" ? searchTitle.innerHTML = "Please enter a new search": searchTitle.innerHTML = "You searched for: " + queryString;
-  searchTitle.href = "https://en.wikipedia.org/wiki/" + rdyString;
-}
-
-function styleContainer() {
-  container.style.backgroundColor = "rgb(93, 51, 110)";
-  container.style.border = "2px solid white";
-}
-
-function addScrollBtn() {
-  scrollBtn.innerHTML = "<a href='#text'><i class='fa fa-arrow-circle-up fa-1' aria-hidden='true'></i></a>";
-}
